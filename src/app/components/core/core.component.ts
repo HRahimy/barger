87550b09f15ca81b93447c69ae7ed28a608d7811
HRahimy <rahimy.hamza@gmail.com>
2022-04-14 16:25:14 +0300

    WIP currency conversion logic

--- src/app/components/core/core.component.ts
+++ src/app/components/core/core.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {ICostsResponse} from '../../interfaces/costs-response.interface';
-import {IExchangeRate} from '../../interfaces/exchange-rate.interface';
+import {IExchangeRate, IExchangeRatePaymentCurrency} from '../../interfaces/exchange-rate.interface';
 import {ICurrency} from '../../interfaces/currency.interface';
 import {ICost} from '../../interfaces/cost.interface';
 
@@ -40,8 +40,52 @@ export class CoreComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  updateDaCurrency(currency: ICurrency): void {
-    console.log(`triggering update currency handler with currency: ${currency.currency}...`);
+  updateDaCurrency(selectedCurrency: ICurrency): void {
+    console.log(`triggering update currency handler with currency: ${selectedCurrency.currency}...`);
+    const currentPaymentCurrencyRates = this.exchangeRate.paymentCurrencies;
+    let newPaymentCurrencyRates: IExchangeRatePaymentCurrency[] = [];
+    let newCurrencyOptions: ICurrency[] = [];
+
+    const rateFromPrevToSelected = 1 / selectedCurrency.exchangeRate!;
+    console.log(`rate from previous to selected: ${rateFromPrevToSelected}`)
+
+    for (let i = 0; i < currentPaymentCurrencyRates.length; i++) {
+      let newCurrencyRate: IExchangeRatePaymentCurrency;
+      let newCurrencyOption: ICurrency;
+      if (currentPaymentCurrencyRates[i].toCurrency === this.exchangeRate.sourceCurrency) {
+        newCurrencyRate = {
+          fromCurrency: selectedCurrency.currency,
+          toCurrency: currentPaymentCurrencyRates[i].toCurrency,
+          exchangeRate: rateFromPrevToSelected
+        }
+        newCurrencyOption = {
+          currency: currentPaymentCurrencyRates[i].toCurrency,
+          exchangeRate: rateFromPrevToSelected
+        }
+      } else {
+        newCurrencyRate = {
+          fromCurrency: selectedCurrency.currency,
+          toCurrency: currentPaymentCurrencyRates[i].toCurrency,
+          exchangeRate: selectedCurrency.currency === currentPaymentCurrencyRates[i].toCurrency
+            ? 1
+            : currentPaymentCurrencyRates[i].exchangeRate / rateFromPrevToSelected
+        };
+        newCurrencyOption = {
+          currency: currentPaymentCurrencyRates[i].toCurrency,
+          exchangeRate: newCurrencyRate.exchangeRate
+        };
+      }
+      newPaymentCurrencyRates = [...newPaymentCurrencyRates, newCurrencyRate];
+      newCurrencyOptions = [...newCurrencyOptions, newCurrencyOption];
+    }
+
+    this.exchangeRate = <IExchangeRate>{
+      sourceCurrency: selectedCurrency.currency,
+      paymentCurrencies: [...newPaymentCurrencyRates]
+    };
+    this.currencyOptions = [...newCurrencyOptions];
+
+    console.log(this.exchangeRate.paymentCurrencies);
     // Using `for` loop instead of `this.exchangeRate.paymentCurrencies.forEach()`
     // for better performance.
     // const paymentCurrencies = this.exchangeRate.paymentCurrencies;
